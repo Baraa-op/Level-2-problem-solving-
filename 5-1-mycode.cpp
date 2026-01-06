644e46aecd60d207b653dd51409777bddc2d18b9
Baraa <156128855+Baraa-op@users.noreply.github.com>
2026-01-06 22:14:56 +0300

    Implement functions to read and print digits

---
+++ 5-1-mycode.cpp
@@ -0,0 +1,46 @@
+//Advanced-2 - 2
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std  ;
+
+
+
+int Read_Positive_Numbers(string Message)
+{
+    
+    int Number = 0   ;
+    do
+    {
+        cout << Message ;
+        cin >> Number;
+    
+    }while(Number <= 0 );
+    
+    return Number;
+
+
+}
+
+
+int Print_Digits(int Number)
+{
+    //int Sum = 0 ;
+    int Remainder = 0;
+    while(Number > 0)
+    {
+        Remainder = Number % 10 ;
+        Number = Number / 10 ;
+        cout << Remainder << endl;
+    }
+}
+
+
+
+
+
+
+int main()
+{
+    Print_Digits(Read_Positive_Numbers("plese enter Number : "));
+}
