b372f1a2927b0a7af82cb4d24f1558855b359d8b
Baraa <156128855+Baraa-op@users.noreply.github.com>
2026-01-08 12:43:21 +0300

    Create 13mycode.cpp

---
+++ 13mycode.cpp
@@ -0,0 +1,54 @@
+//Advanced-9- 2
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std  ;
+
+
+
+int Read_Positive_Numbers(string Message)
+{
+    
+    int Number = 0   ;
+    do
+    {
+        cout << Message ;
+        cin >> Number;
+        cout << "\n" ;
+    
+    }while(Number <= 0 );
+    
+    return Number;
+
+
+}
+
+
+
+
+
+void Print_Pattern(int Number)
+{
+    for(int i = 1 ; i <= Number; i++)
+    {
+        for(int j = 1 ; j <= i ; j++)
+        {
+            cout << i ;
+        }
+        cout << "\n" ;
+    }
+}
+
+
+
+
+
+
+
+int main()
+{
+   int Number = Read_Positive_Numbers("plese enter Number : " );
+   Print_Pattern(Number);
+}
+
+    
