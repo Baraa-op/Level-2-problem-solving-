44e9d75dd9e9ef65f2f581beb58f2ddae1aee7ee
Baraa <156128855+Baraa-op@users.noreply.github.com>
2026-01-10 15:24:15 +0300

    Create 24-pablo.cpp

---
+++ 24-pablo.cpp
@@ -0,0 +1,88 @@
+//Advanced-24- 2
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+using namespace std  ;
+
+int Read_Positive_Numbers(string Message)
+{
+
+    int Number = 0   ;
+    do
+    {
+        cout << Message ;
+        cin >> Number;
+        cout << "\n" ;
+
+    } while(Number <= 0 );
+
+    return Number;
+
+
+}
+
+int Random_Number(int From , int To)
+{
+    int Random = rand() % (To - From + 1) + From ;
+    return Random ;
+}
+
+
+
+void Random_Array(int Arr[100], int &ArrLength)
+{
+
+    cout << "Plese enter length of array : " ;
+    cin >> ArrLength;
+
+
+    for(int i = 0 ; i<ArrLength ; i++)
+    {
+        Arr[i] = Random_Number(1 , 100) ;
+    }
+    cout <<"\n" ;
+
+}
+
+void Print_Array(int Arr[100], int ArrLength)
+{
+    for(int i = 0 ; i<ArrLength ; i++)
+    {
+        cout << Arr[i] << " " ;
+    }
+}
+
+
+int Max_Of_Array(int Arr[100], int ArrLength)
+{
+    int Max = Arr[0] ;
+    int Counter = 0 ;
+    for(int i = 1; i < ArrLength ; i++)
+    {
+        Counter = Arr[i] ;
+        if(Max <= Counter)
+        {
+            Max = Arr[i] ;
+        }
+    }
+    
+    return Max ;
+}
+
+
+int main()
+{
+
+    srand((unsigned)time(NULL));
+    //int Number = Read_Positive_Numbers("plese enter Number : ");
+
+    int ArrLength ;
+    int Arr[100] ;
+    cout << "\n" ;
+    Random_Array(Arr, ArrLength) ;
+    Print_Array(Arr, ArrLength);
+    cout << "\n" ;
+    cout << "Max Of Array is : " << Max_Of_Array(Arr, ArrLength) ;
+   
+}   
